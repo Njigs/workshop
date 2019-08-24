4ab1e18e7ad8339804cbb0b2b6ced70dc2d37e39
Njigs <fnjiguawa@gmail.com>
2019-08-24 14:49:27 +0300

    created form to validate driver age and the type of truck

---
+++ njigs.js
@@ -0,0 +1,8 @@
+let age=prompt("please enter your age");
+if (age>=18){
+    console.log("you can drive a small car");
+}else if (age>=24){
+    console.log("you can drive a truck");
+}else if (age<18){
+    console.log("you are too young to drive");
+}
---
+++ njigs2.js
@@ -0,0 +1,11 @@
+function drive(event){
+    event preventdefault();
+    let age=
+    if (age>=18 && age <=24){
+        console.log("you can drive a small car");
+    }
+    else if (age>24){
+        console.log("you can drive a truck");
+    }
+
+}
